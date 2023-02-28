280219826572713b252b008a5739a338c0ffe58f
SevillaChristian <67481106+SevillaChristian@users.noreply.github.com>
2023-02-27 20:57:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+//variables
+let contador = 0;
+const parrafo = document.getElementById('demo');
+const boton_dis = document.getElementById('bt-1');
+const boton_rei = document.getElementById('bt-2');
+const boton_aum = document.getElementById('bt-3');
+/*
+console.log(parrafo);
+console.log(boton_dis);
+console.log(boton_rei);
+console.log(boton_aum);*/
+//eventos de accion por cada boton 
+boton_dis.addEventListener('click', contador_dis);
+boton_rei.addEventListener('click', contador_rei);
+boton_aum.addEventListener('click', contador_aum);
+
+//declaracion de las funciones
+function contador_dis(){
+    --contador
+    colorContador(contador)
+    return(parrafo.innerText = contador);
+}
+function contador_rei(){
+    contador = 0;
+    colorContador(contador)
+    return(parrafo.innerText = contador);
+}
+function contador_aum(){
+    ++contador
+    colorContador(contador)
+    return(parrafo.innerText = contador);
+}
+function colorContador(value){
+    let resultado;
+    if(value >0){
+        resultado = parrafo.style.color = 'darkgreen';
+    }
+    if (value ==0) {
+        resultado = parrafo.style.color = 'black';
+    }
+    if (value < 0) {
+        resultado = parrafo.style.color = 'red';
+    }
+    return resultado
+}
